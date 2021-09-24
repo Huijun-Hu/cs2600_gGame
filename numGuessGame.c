0c7f4c9c0787d65440902de6882782651fa33c82
Huijun Hu <huijunhu@cpp.edu>
2021-09-23 22:08:22 -0700

    Initial commit - outline

---
+++ numGuessGame.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+Number Guessing Game
+@ author : Huijun Hu
+ - dispaly game menu to user. 
+ - 1 : start game
+ - 2 : change max limit 
+ - 3 : quit
+*/
